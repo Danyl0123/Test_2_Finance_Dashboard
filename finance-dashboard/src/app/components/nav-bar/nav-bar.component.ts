f2f72ca271220a08b008b67eb7c50858dbeda998
Danylo Zamkovyi <25032002ur@gmail.com>
2024-07-18 09:13:00 +0300

    add routes

--- finance-dashboard/src/app/components/nav-bar/nav-bar.component.ts
+++ finance-dashboard/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  constructor(private router: Router) {}
+
+  navigateToShortInformation() {
+    this.router.navigate(['/short-information']);
+  }
+  navigateToMainTable() {
+    this.router.navigate(['']);
+  }
+}
--- finance-dashboard/src/app/pages/short-information/short-information.component.ts
+++ finance-dashboard/src/app/pages/short-information/short-information.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TableComponent } from '../../components/table/table.component';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-short-information',
   standalone: true,
-  imports: [TableComponent],
+  imports: [TableComponent, NavBarComponent],
   templateUrl: './short-information.component.html',
   styleUrl: './short-information.component.css',
 })
